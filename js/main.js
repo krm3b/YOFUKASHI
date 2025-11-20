7844315649917f4ab58159be31b1d0d938628725
miyu <sunrout32@icloud.com>
2025-11-21 01:51:16 +0900

    お悩み、プロナビとはを作成しました

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 $(function () {
-
+    /*=================================================
+    PICK UP スライダー
+    ===================================================*/
+    // カルーセル用 jQueryプラグイン「slick」の設定
+    // マニュアル：https://kenwheeler.github.io/slick/
+    $(".slick").slick({
+        centerMode: true,                 //両端見切れ状態の有無
+        slidesToShow: 3,                  //1スライダーの表示数
+        autoplay: true,                   //自動再生の有無
+        autoplaySpeed: 0,                 //再生速度（1000=1秒）
+        speed: 7000,                      // スライドが流れる速度を設定
+        cssEase: "linear",                // スライドの流れ方を等速に設定
+        responsive: [
+            {
+              breakpoint: 769, // 769px以下のとき
+                settings: {
+                speed: 8000,
+                slidesToShow: 2,
+                centerMode: false,
+            },
+            },
+            {
+              breakpoint: 426, // 426px以下のとき
+                settings: {
+                speed: 5000,
+                slidesToShow: 1,
+                centerMode: false,
+            },
+            },
+        ],
+    });
+    
     /*=================================================
         FAQ
     ===================================================*/
@@ -25,80 +56,5 @@ $(function () {
             $(Box).slideDown(500);					//アコーディオンを開く
         });
     });
-
-    /*=================================================
-        FAOの背景　パーティクルズJS
-    ===================================================*/
-    particlesJS("particles-js", {
-        "particles":{
-            "number":{
-                "value":40,     //この数値を変更すると幾何学模様の数が増減できる
-                "density":{"enable":true,"value_area":800}
-            },
-            "color":{
-                "value":"#3688d6"
-            },
-            "shape":{
-                "type":"polygon",       //形状はpolygonを指定
-                "stroke":{"width":0},
-                "polygon":{"nb_sides":5},
-                "image":{"width":100,"height":100}
-            },
-            "opacity":{
-                "value":0.5,
-                "random":false,
-                "anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}
-            },
-            "size":{
-                "value":4.008530152163807,
-                "random":true,
-                "anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}
-            },
-            "line_linked":{
-                "enable":true,
-                "distance":150,
-                "color":"#3688d6",
-                "opacity":0.4,
-                "width":1
-            },
-            "move":{
-                "enable":true,
-                "speed":3,              //この数値を小さくするとゆっくりな動きになる
-                "direction":"none",     //方向指定なし
-                "random":false,         //動きはランダムにしない
-                "straight":false,       //動きをとどめない
-                "out_mode":"out",       //画面の外に出るように描写
-                "bounce":false,         //跳ね返りなし
-                "attract":{"enable":false,"rotateX":600,"rotateY":1200}
-            }
-        },
-        "interactivity":{
-            "detect_on":"canvas",
-            "events":{
-                "onhover":{"enable":true,"mode":"repulse"},
-                "onclick":{"enable":true,"mode":"push"},
-                "resize":true
-            },
-            "modes":{
-                "grab":{"distance":400,"line_linked":{"opacity":1}},
-                "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
-                "repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},
-                "remove":{"particles_nb":2}
-            }
-        },
-        "retina_detect":true
-    });
-    var count_particles, stats, update; stats = new Stats; stats.setMode(0); 
-    stats.domElement.style.position = 'absolute'; 
-    stats.domElement.style.left = '0px'; 
-    stats.domElement.style.top = '0px'; 
-    document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); 
-    update = function() { stats.begin(); stats.end(); 
-        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { 
-            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; 
-        } 
-        requestAnimationFrame(update); 
-    }; 
-    requestAnimationFrame(update);;
 
 });
